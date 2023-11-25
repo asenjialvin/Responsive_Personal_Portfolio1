c0952babf9e9740fe9d16606b2ba741d17b3521b
asenjialvin <asenjialvin@gmail.com>
2023-11-25 13:22:29 +0300

    Added a primary background color to the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Header from "./components/Header/Header";
 import css from './styles/app.module.scss'
 
+
 const App = () => {
-  return <div className={css.container}>
+  return <div className={`bg-primary ${css.container}`}>
     <Header/>
   </div>;
 };
